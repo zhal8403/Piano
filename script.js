9aae495bd72ce26ae0ad399095e8a6bdb87d7b52
zhal8403 <zhal8403@gmail.com>
2026-06-28 06:12:14 -0400

    starts js that continuously asks your ESP32 which keys are being pressed and updates the piano display on the webpage.

---
+++ script.js
@@ -0,0 +1,39 @@
+const status = document.getElementById("status");
+
+const notes = {1: 261.63, 2: 293.66, 3: 329.63, 4: 349.23};
+
+function updateKeys(data)
+{
+    for(let i = 1; i <= 4; i++)
+    {
+        const key = document.getElementById("key" + i)
+
+        if (data[i] == 1)
+        {
+            key.classList.add("active");
+        }
+        else
+        {
+            key.classList.remove("active");
+        }
+    }
+}
+
+function getKeys()
+{
+    try
+    {
+        const response = await fetch("/keys");
+        const data = await response.json();
+
+        status.innerHTML = "🟢 connected";
+
+        updateKeys(data);
+    }
+    catch
+    {
+        status.innerHTML = "🔴 Disconnected";
+    }
+}
+
+setInterval(getKeys, 20);
